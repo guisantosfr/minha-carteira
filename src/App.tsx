434a26924941da190d109c7599d99779e68ae125
Guilherme Santos <santosgui678@gmail.com>
2022-01-03 10:45:20 -0300

    Adicionando estilos globais

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+import GlobalStyles from './styles/GlobalStyles';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
