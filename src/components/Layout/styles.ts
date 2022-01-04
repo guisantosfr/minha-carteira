9ed4ce96779d14d3d27459331a3dd65e807f49a6
Guilherme Santos <santosgui678@gmail.com>
2022-01-04 10:26:40 -0300

    Adicionando componentes de layout e temas de cores

---
+++ src/components/Layout/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 15rem auto;
+  grid-template-rows: 5rem auto;
+  grid-template-areas: 'AS MH' 'AS CT';
+  height: 100vh;
+  `;
