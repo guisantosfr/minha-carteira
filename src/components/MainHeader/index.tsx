fe38a2c71d36ce4ffdfa6b1dbfd73db3b76aa538
Guilherme Santos <santosgui678@gmail.com>
2022-01-05 11:02:01 -0300

    Criando Header principal e toggle

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useMemo } from 'react';
+import { Container, Profile, Welcome, Username } from './styles';
+
+import emojis from '../../utils/emojis';
+import Toggle from '../Toggle';
 
 const MainHeader: React.FC = () => {
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * emojis.length);
+    return emojis[index];
+  }, []);
+
   return (
     <Container>
-      <h1>MainHeader</h1>
+      <Toggle />
+
+      <Profile>
+        <Welcome>Olá, {emoji}</Welcome>
+        <Username>Guilherme Santos</Username>
+      </Profile>
     </Container>
   );
 }
