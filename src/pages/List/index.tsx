7ae418abe234ab0da6fc6c57e7b49a6cf096c138
Guilherme Santos <santosgui678@gmail.com>
2022-01-08 10:39:47 -0300

    Implementando filtros

--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -4,18 +4,32 @@ import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 import HistoryFinanceCard from '../../components/HistoryFinanceCard';
 
-import { Container, Content } from './styles';
+import { Container, Content, Filters } from './styles';
 
 const List: React.FC = () => {
-  const options = [
-    { value: '1', label: '1' }
+  const months = [
+    { value: 1, label: 'Janeiro' },
+    { value: 2, label: 'Fevereiro' },
+    { value: 3, label: 'Março' },
+  ];
+
+  const years = [
+    { value: 2022, label: 2022 },
+    { value: 2021, label: 2021 },
+    { value: 2020, label: 2020 },
   ];
 
   return (
     <Container>
       <ContentHeader title="Saídas" lineColor='#FFF'>
-        <SelectInput options={options} />
+        <SelectInput options={months} />
+        <SelectInput options={years} />
       </ContentHeader>
+
+      <Filters>
+        <button type='button' className='tag-filter tag-filter-recurrent'>Recorrentes</button>
+        <button type='button' className='tag-filter tag-filter-eventual'>Eventuais</button>
+      </Filters>
 
       <Content>
         <HistoryFinanceCard
--- src/pages/List/styles.ts
+++ src/pages/List/styles.ts
@@ -7,3 +7,41 @@ export const Container = styled.div`
 export const Content = styled.div`
 
 `;
+
+export const Filters = styled.div`
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+
+  margin-bottom: 2rem;
+
+  .tag-filter{
+    font-size: 1.125rem;
+    font-weight: 500;
+    background: none;
+    color: ${props => props.theme.color.white};
+    margin: 0 1.25rem;
+    transition: opacity .3s;
+
+    &:hover{
+      opacity: .7;
+    }
+  }
+
+  .tag-filter-recurrent::after{
+      content: '';
+      display: block;
+      width: 3.5rem;
+      margin: 0 auto;
+      border-bottom: 10px solid ${props => props.theme.color.warning};
+  }
+
+  .tag-filter-eventual::after{
+      content: '';
+      display: block;
+      width: 3.5rem;
+      margin: 0 auto;
+      border-bottom: 10px solid ${props => props.theme.color.warning};
+  }
+`;
