86f54286fd9d3db26b2981cd15dd9ffeeea80890
Guilherme Santos <santosgui678@gmail.com>
2022-05-21 11:20:43 -0300

    Header do dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,19 +1,79 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 
 import ContentHeader from '../../components/ContentHeader';
 import SelectInput from '../../components/SelectInput';
 
+import expenses from '../../repositories/expenses';
+import gains from '../../repositories/gains';
+import monthsList from '../../utils/months';
+
 import { Container } from './styles';
 
 const Dashboard: React.FC = () => {
-  const options = [
-    { value: '1', label: '1' }
-  ];
+  const [monthSelected, setMonthSelected] = useState<number>(new Date().getMonth() + 1);
+  const [yearSelected, setYearSelected] = useState<number>(new Date().getFullYear());
+
+  const months = useMemo(() => {
+    return monthsList.map((month, index) => {
+      return {
+        value: index + 1,
+        label: month
+      }
+    });
+  }, []);
+
+  const years = useMemo(() => {
+    let uniqueYears: number[] = [];
+
+    [...expenses, ...gains].forEach(item => {
+      const date = new Date(item.date);
+      const year = date.getFullYear();
+
+      if (!uniqueYears.includes(year)) {
+        uniqueYears.push(year);
+      }
+    });
+
+    return uniqueYears.map(year => {
+      return {
+        value: year,
+        label: year
+      }
+    });
+  }, []);
+
+  const handleMonthSelected = (month: string) => {
+    try {
+      const parseMonth = Number(month);
+      setMonthSelected(parseMonth);
+    } catch (error) {
+      throw new Error('invalid month value.')
+    }
+  };
+
+  const handleYearSelected = (year: string) => {
+    try {
+      const parseYear = Number(year);
+      setYearSelected(parseYear);
+    } catch (error) {
+      throw new Error('invalid year value.')
+    }
+  };
 
   return (
     <Container>
-      <ContentHeader title="Dashboard" lineColor='#FFF'>
-        <SelectInput options={options} onChange={() => { }} />
+      <ContentHeader title="Dashboard" lineColor='#F7931B'>
+        <SelectInput
+          options={months}
+          onChange={(e) => handleMonthSelected(e.target.value)}
+          defaultValue={monthSelected}
+        />
+
+        <SelectInput
+          options={years}
+          onChange={(e) => handleYearSelected(e.target.value)}
+          defaultValue={yearSelected}
+        />
       </ContentHeader>
     </Container>
   );
--- src/pages/List/index.tsx
+++ src/pages/List/index.tsx
@@ -35,7 +35,7 @@ const List: React.FC = () => {
   const pageData = useMemo(() => {
     return movimentType === 'entry-balance' ? {
       title: 'Entradas',
-      lineColor: '#F7931B',
+      lineColor: '#4E41F0',
       data: gains
     } : {
       title: 'Saídas',
