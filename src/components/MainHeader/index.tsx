93b16f63eabae0aef8d3e049b09db3ffd3384bfb
Guilherme Santos <santosgui678@gmail.com>
2022-05-29 13:05:22 -0300

    Toggle dinâmico + persistência do tema

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,10 +1,20 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import { Container, Profile, Welcome, Username } from './styles';
 
 import emojis from '../../utils/emojis';
 import Toggle from '../Toggle';
 
+import { useTheme } from '../../hooks/theme';
+
 const MainHeader: React.FC = () => {
+  const { toggleTheme, theme } = useTheme();
+  const [darkTheme, setDarkTheme] = useState(() => theme.title === 'dark' ? true : false);
+
+  const handleThemeChange = () => {
+    setDarkTheme(!darkTheme);
+    toggleTheme();
+  }
+
   const emoji = useMemo(() => {
     const index = Math.floor(Math.random() * emojis.length);
     return emojis[index];
@@ -12,7 +22,12 @@ const MainHeader: React.FC = () => {
 
   return (
     <Container>
-      <Toggle />
+      <Toggle
+        labelLeft="Light"
+        labelRight="Dark"
+        checked={darkTheme}
+        onChange={handleThemeChange}
+      />
 
       <Profile>
         <Welcome>Olá, {emoji}</Welcome>
